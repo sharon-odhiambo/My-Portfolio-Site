ff79742d79946482e3d034ab0d559377a2b0ce67
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-20 23:47:21 +0300

    Add menu button hide and show interactions

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const hamburgerButton= document.querySelector('.menu-button');
-const exitButton= document.querySelector('.exit-btn');
-const navButton= document.querySelector('.mobile-nav-menu');
-const navOptions= document.querySelectorAll('mobile-nav-link');
-
-menu-button.addEventListener('click', function(event) {
-mobile-nav-menu.style.display = 'none';
-})
+const hamburgerButton = document.querySelector('.menu-button');
+const navButton = document.querySelector('.mobile-nav-menu');
+const exitButton = document.querySelector('.exit-btn');
+const navOptions = document.querySelectorAll('.mobile-nav-link');
+const headSection = document.querySelector('.header-background')
+// Open Menu Bar
+hamburgerButton.addEventListener('click', () => {
+  navButton.style.display = ('block');
+  exitButton.style.display = ('block');
+  headSection.style.backgroundColor = ('#6070ff');
+});
+// Close Menu Bar
+exitButton.addEventListener('click', () => {
+    navButton.style.display = ('none');
+    exitButton.style.display = ('none');
+    headSection.style.backgroundColor = ('#ffffff');
+});
+// navOptions.addEventListener('click' () => {
+//     navButton.style.display = ('none');
+// });
