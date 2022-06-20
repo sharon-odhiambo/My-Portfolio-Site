af1dfbd800a7e2b64399c44c55c3f454e378fc68
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-20 18:41:07 +0300

    Add hamburger variables

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburgerButton= document.querySelector('.menu-button');
+const exitButton= document.querySelector('.exit-btn');
+const navButton= document.querySelector('.mobile-nav-menu');
+const navOptions= document.querySelectorAll('mobile-nav-link');
+
+menu-button.addEventListener('click', function(event) {
+mobile-nav-menu.style.display = 'none';
+})
