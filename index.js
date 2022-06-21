aa01f3285bdc4cf4f304caf6868ff3f8cfe773a0
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-21 13:34:49 +0300

    Add hamburger interactions and fix linter errors

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const hamburgerButton = document.querySelector('.menu-button');
 const navButton = document.querySelector('.mobile-nav-menu');
 const exitButton = document.querySelector('.exit-btn');
-const navOptions = document.querySelectorAll('.mobile-nav-link');
-const headSection = document.querySelector('.header-background')
+const navOptions = document.querySelectorAll('.mobile-nav-link a');
+const headSection = document.querySelector('.header-background');
 // Open Menu Bar
 hamburgerButton.addEventListener('click', () => {
   navButton.style.display = ('block');
   exitButton.style.display = ('block');
-  headSection.style.backgroundColor = ('#6070ff');
+  headSection.style.backgroundColor = ('rgb(0,0,255,0.6)');
+  hamburgerButton.style.display = ('none');
 });
 // Close Menu Bar
 exitButton.addEventListener('click', () => {
-    navButton.style.display = ('none');
-    exitButton.style.display = ('none');
-    headSection.style.backgroundColor = ('#ffffff');
+  navButton.style.display = ('none');
+  exitButton.style.display = ('none');
+  headSection.style.backgroundColor = ('#ffffff');
+  hamburgerButton.style.display = ('block');
 });
-// navOptions.addEventListener('click' () => {
-//     navButton.style.display = ('none');
-// });
+// Close Menu Bar Using navOptions
+navOptions.addEventListener('click', () => {
+  navButton.style.display = ('none');
+  exitButton.style.display = ('none');
+});
