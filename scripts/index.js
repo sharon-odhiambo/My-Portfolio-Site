c5669ea0e7e36e0b7921959bf8405186f846fcb1
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-23 19:09:44 +0300

    Add closing buttons

--- scripts/index.js
+++ scripts/index.js
@@ -26,18 +26,18 @@ navOptions.forEach((navOption) => {
     hamburgerButton.style.display = ('block');
   });
 });
-// Works-Projects popup-window
-const projectSection = [
+//Works-Porjects popup-window
+const worksection = [
   {
-    id: 1,
-    title: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: './images/SnapshootPortfolio.svg',
-    altmessage: 'Tonic-project',
-    projects_overview: ['CANOPY', 'Full Stack Dev', '2022'],
-    technologies: ['html', 'css', 'javaScript'],
-    seeLive: 'https://sharon-odhiambo.github.io/',
-    seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
+  id: 1,
+  title: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: './images/SnapshootPortfolio.svg',
+  altmessage: 'Tonic-project',
+  projects_overview: ['CANOPY', 'Full Stack Dev', '2022'],
+  technoligies: ['html', 'css', 'javaScript'],
+  seeLive: 'https://sharon-odhiambo.github.io/',
+  seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
   },
   {
     id: 2,
@@ -45,21 +45,21 @@ const projectSection = [
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: './images/SnapshootPortfolio1.svg',
     altMessage: 'Multi-Post stories project',
-    projectsOverview: ['CANOPY', 'Full Stack Dev', '2022'],
-    technologies: ['html', 'css', 'javaScript'],
+    projects_overview: ['CANOPY', 'Full Stack Dev', '2022'],
+    technoligies: ['html', 'css', 'javaScript'],
     seeLive: 'https://sharon-odhiambo.github.io/',
     seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
   },
   {
-    id: 3,
-    title: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: './images/SnapshootPortfolio2.svg',
-    altMessage: 'Tonic-project',
-    projectsOverview: ['CANOPY', 'Full Stack Dev', '2022'],
-    technologies: ['html', 'css', 'javaScript'],
-    seeLive: 'https://sharon-odhiambo.github.io/',
-    seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
+  id: 3,
+  title: 'Tonic',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: './images/SnapshootPortfolio2.svg',
+  altMessage: 'Tonic-project',
+  projects_overview: ['CANOPY', 'Full Stack Dev', '2022'],
+  technoligies: ['html', 'css', 'javaScript'],
+  seeLive: 'https://sharon-odhiambo.github.io/',
+  seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
   },
   {
     id: 4,
@@ -67,142 +67,89 @@ const projectSection = [
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: './images/SnapshootPortfolio3.svg',
     altMessage: 'Multi-Post stories-project',
-    projectsOverview: ['CANOPY', 'Full Stack Dev', '2022'],
-    technologies: ['html', 'css', 'javaScript'],
+    projects_overview: ['CANOPY', 'Full Stack Dev', '2022'],
+    technoligies: ['html', 'css', 'javaScript'],
     seeLive: 'https://sharon-odhiambo.github.io/',
     seeSource: 'https://github.com/sharon-odhiambo/My-Portfolio-Site',
-  },
+  }
 ];
-// // Create Works Section
-function createWorks(projectSection) {
+// Create Works Section
+function createWorks(worksection) {
   console.log('Sharon');
   const workSection = document.querySelector('#works-sect');
-  for(i = 0; i < projectSection.length; i++) {
+  for(i = 0; i < worksection.length; i++) {
     const doc = `
     <div class="works-project">
     <div class="works-top">
-    <div class="snapshoot-${projectSection[i].id}"></div>
+    <div class="snapshoot-${worksection[i].id}"></div>
     </div>
     <div class="project-info">
-    <h2>${projectSection[i].title}</h2>
+    <h2>${worksection[i].title}</h2>
     <ul class="project-details">
-        <li>${projectSection[i].projectsOverview[0]}</li>
-        <li>${projectSection[i].projectsOverview[1]}</li>
-        <li>${projectSection[i].projectsOverview[2]}</li>
+        <li>${worksection[i].projects_overview[0]}</li>
+        <li>${worksection[i].projects_overview[1]}</li>
+        <li>${worksection[i].projects_overview[2]}</li>
     </ul>
     <div>
-      <p>${projectSection[i].description}.</p>
+      <p>${worksection[i].description}.</p>
     </div>
     <div>
     <ul class="languages">
-      <li>${projectSection[i].technologies[0]}</li>
-      <li>${projectSection[i].technologies[1]}</li>
-      <li>${projectSection[i].technologies[2]}</li>
+      <li>${worksection[i].technoligies[0]}</li>
+      <li>${worksection[i].technoligies[1]}</li>
+      <li>${worksection[i].technoligies[2]}</li>
     </ul>
     </div>
-    <button type="button" class="button" id="btnSeeProject${projectSection[i].id}"><span>See Project ${projectSection[i].id}</span></button>
+    <button type="button" class="button" id="btnSeeProject${worksection[i].id}"><span>See Project ${worksection[i].id}</span></button>
   </div>
   </div>`;
-  }
   workSection.innerHTML += doc;
+  }
 }
 // const popupButton = document.getElementById('popup');
-document.addEventListener('DomContentLoaded', createWorks);
-
-
-// const popUpWindow = document.querySelector('.mobile-popup');
-// function popUp(index) {
-//   popUpWindow.classList.add('popUp');
-//   document.querySelector('#popup-header').innerHTML = projects[index].id;
-//   document.querySelector('#popup-header').innerHTML = projects[index].title;
-//   document.querySelector('#popup-description').innerHTML = projects[index].description;
-//   document.querySelector('#popup-image').src = `${projects[index].featuredImage}`;
-//   document.querySelector('#popup-image-alt').innerHTML = projects[index].altMessage;
-//   document.querySelector('#popup-overview').innerHTML = projects[index].projectsOverview;
-//   document.querySelector('#popup-technologies').innerHTML = projects[index].technologies;
-//   document.querySelector('#seeLivePopup').href = projects[index].seeLive;
-//   document.querySelector('#seeSourcePopup').href = projects[index].seeSource;
-// }
-// function popUpClose() {
-//   popUpWindow.classList.remove('popUp');
-// }
-// const closePopUpWindow = document.querySelector('#closePopup');
-// closePopUpWindow.addEventListener('click', popUpClose);
-// const seeProject = document.querySelectorAll('.button');
-// seeProject.forEach((element) => {
-//   element.addEventListener('click', () => {
-//     popUp(element.dataset.id);
+document.addEventListener('DomContentLoaded', createWorks(worksection));
+// // Create Popup window
+// const Popup = document.querySelector('#mobile-popup');
+// const openPopups = document.querySelectorAll('.button');
+// const closeButton = document.querySelector('#close-popup');
+// // Open Popup Window
+// openPopups.forEach((openPopup) => {
+//   openPopup.addEventListener('click', () => {
+//     button.addEventListener('click', () => {
+//       document.body.style.position = 'fixed';
+//       document.body.style.overflow = 'hidden';
+//       popUpWindow.classList.remove('hide');
 //   });
 // });
-// function createWorks(myArray) {
-//   const projectSection = document.querySelector('#works-sect');
-//   for(i = 0; i < myArray.length; i++) {
-//     const doc = `
-//     <div class="works-project">
-//     <div class="works-top">
-//     <div class="snapshoot-one"></div>
-//     </div>
-//     <div class="project-info">
-//     <h2>Tonic</h2>
-//     <ul class="project-details">
-//         <li>Canopy</li>
-//         <li>BackEnd Dev</li>
-//         <li>2015</li>
-//     </ul>
-//     <div>
-//       <p>A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-//     </div>
-//     <div>
-//     <ul class="languages">
-//       <li>html</li>
-//       <li>css</li>
-//       <li>javascript</li>
-//     </ul>
-//     </div>
-//     <button type="button" class="button"><span>See Project</span></button>
-//   </div>
-//   </div>`;
-//   projectSection += doc;
-//   }
-// }
-// const popupButton = document.getElementById('popup');
-// popupButton.addEventListener('click', createWorks(projectSection));
-
-// const popUpWindow = document.querySelector('.mobile-popup');
-// function popUp(index) {
-//   popUpWindow.classList.add('popUp');
-//   document.querySelector('#popup-header').innerHTML = projects[index].id;
-//   document.querySelector('#popup-header').innerHTML = projects[index].title;
-//   document.querySelector('#popup-description').innerHTML = projects[index].description;
-//   document.querySelector('#popup-image').src = `${projects[index].featuredImage}`;
-//   document.querySelector('#popup-image-alt').innerHTML = projects[index].altMessage;
-//   document.querySelector('#popup-overview').innerHTML = projects[index].projectsOverview;
-//   document.querySelector('#popup-technologies').innerHTML = projects[index].technologies;
-//   document.querySelector('#seeLivePopup').href = projects[index].seeLive;
-//   document.querySelector('#seeSourcePopup').href = projects[index].seeSource;
-// }
-
-// // const popUpWindow = document.querySelector('.popup-window');
-// // function popUp(index) {
-// //   popUpWindow.classList.add('popUp');
-// //   document.querySelector('#popup-header').innerHTML = projects[index].id;
-// //   document.querySelector('#popup-header').innerHTML = projects[index].title;
-// //   document.querySelector('#popup-description').innerHTML = projects[index].description;
-// //   document.querySelector('#popup-image').src = `${projects[index].featuredImage}`;
-// //   document.querySelector('#popup-image-alt').innerHTML = projects[index].altMessage;
-// //   document.querySelector('#popup-overview').innerHTML = projects[index].projectsOverview;
-// //   document.querySelector('#popup-technologies').innerHTML = projects[index].technologies;
-// //   document.querySelector('#seeLivePopup').href = projects[index].seeLive;
-// //   document.querySelector('#seeSourcePopup').href = projects[index].seeSource;
-// // }
-// // function popUpClose() {
-// //   popUpWindow.classList.remove('popUp');
-// // }
-// // const closePopUpWindow = document.querySelector('#closePopup');
-// // closePopUpWindow.addEventListener('click', popUpClose);
-// // const seeProject = document.querySelectorAll('.button');
-// // seeProject.forEach((element) => {
-// //   element.addEventListener('click', () => {
-// //     popUp(element.dataset.id);
-// //   });
-// // });
+// });
+// // Close Popup Window
+// closeButton.addEventListener('click', () => {
+//   button.addEventListener('click', () => {
+//     document.body.style.position = 'relative';
+//     document.body.style.overflow = 'scroll';
+//     popUpWindow.classList.add('hide');
+//     // hamburgerButton.style.position = 'absolute';
+//     hamburgerButton.style.top = '0';
+//     hamburgerButton.style.margin = '45px 0 0 0';
+//   });
+// });
+const popUpWindow = document.querySelector('.mobile-popup');
+const seeProject = document.querySelectorAll('.button');
+seeProject.forEach((button) => {
+  button.addEventListener('click', () => {
+    document.body.style.position = 'fixed';
+    document.body.style.overflow = 'hidden';
+    popUpWindow.classList.remove('hide');
+  });
+});
+const closePopup = document.querySelectorAll('#close-popup');
+closePopup.forEach((button) => {
+  button.addEventListener('click', () => {
+    document.body.style.position = 'relative';
+    document.body.style.overflow = 'scroll';
+    popUpWindow.classList.add('hide');
+    // hamburgerButton.style.position = 'absolute';
+    hamburgerButton.style.top = '0';
+    hamburgerButton.style.margin = '45px 0 0 0';
+  });
+});
