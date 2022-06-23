a512154bfeb4f969daf97529552d0329b5a4600a
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-23 19:50:02 +0300

    Update validation code

--- scripts/index.js
+++ scripts/index.js
@@ -27,14 +27,54 @@ navOptions.forEach((navOption) => {
   });
 });
 // Create Form Validation
-// const getInTouch = document.getElementById('form');
-document.querySelector('.submit').addEventListener('click', (event) => {
-const email = document.getElementById('#mail').value;
-const error = document.querySelector('#error-message');
-const newString = (str) => str == str.toLowerCase()
-  if (!newString(email)) {
-    event.preventDefault();
-    error.textMessage = 'Sorry! Please use all lowercase.';
+const button = document.getElementById('#form');
+const email = document.getElementById('#mail');
+const error = document.getElementById('#error-message');
+function inputCheck() {
+  const Email = email.value;
+  const newString = (str) => /^[a-z0-9@.]+$/.test(str) && !/[A-Z]/.test(str);
+  if (!newString(Email)) {
+    error.innerHTML = 'Sorry! Please use all lowercase.';
     error.style.color = 'red';
-  };
+  }
+};
+button.addEventListener('submit', (event) => {
+  event.preventDefault();
+  inputCheck();
 });
+
+// getInTouch.addEventListener('submit', (event) => {
+//   const formRequired = /^[a-z0-9@.]+$/;
+// if (!formRequired.test(email.value)) {
+//   error.innerHTML = 'Sorry! Please use all lowercase.';
+//   error.style.color = 'red';
+//   event.preventDefault();
+// }
+// });
+
+// const form = document.querySelector(‘#form’);
+// const email = document.querySelector(‘#mail’);
+// const errorHolder = document.querySelector(‘#error-message’);
+// function inputCheck() {
+//   // Get the values from the inputs
+//   const userEmail = email.value.trim();
+//   const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str); if (!isLowerCase(userEmail)) {
+//     // show error
+//     // add error class
+//     errorHolder.innerText = ‘Invalid Email! Emails must be written with lowercase characters.’;
+//   }
+// }
+// form.addEventListener(‘submit’, (e) => {
+//   e.preventDefault();
+//   inputCheck();
+// });
+
+
+// // const newString = (str) => str == str.toLowerCase()
+// // document.querySelector('.submit').addEventListener('click', (event) => {
+// //   if (!newString(email)) {
+// //     event.preventDefault();
+// //     error.textMessage = 'Sorry! Please use all lowercase.';
+// //     error.style.color = 'red';
+// //   };
+// // });
