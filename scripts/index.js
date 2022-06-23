1017f785d6504d011c6cb5c1ae014fece7d3fe5e
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-06-23 22:05:36 +0300

    Fix linter errors

--- scripts/index.js
+++ scripts/index.js
@@ -27,54 +27,15 @@ navOptions.forEach((navOption) => {
   });
 });
 // Create Form Validation
-const button = document.getElementById('#form');
-const email = document.getElementById('#mail');
-const error = document.getElementById('#error-message');
-function inputCheck() {
+const button = document.getElementById('form');
+const email = document.getElementById('mail');
+const error = document.getElementById('error-message');
+button.addEventListener('submit', (event) => {
   const Email = email.value;
   const newString = (str) => /^[a-z0-9@.]+$/.test(str) && !/[A-Z]/.test(str);
   if (!newString(Email)) {
     error.innerHTML = 'Sorry! Please use all lowercase.';
     error.style.color = 'red';
+    event.preventDefault();
   }
-};
-button.addEventListener('submit', (event) => {
-  event.preventDefault();
-  inputCheck();
 });
-
-// getInTouch.addEventListener('submit', (event) => {
-//   const formRequired = /^[a-z0-9@.]+$/;
-// if (!formRequired.test(email.value)) {
-//   error.innerHTML = 'Sorry! Please use all lowercase.';
-//   error.style.color = 'red';
-//   event.preventDefault();
-// }
-// });
-
-// const form = document.querySelector(‘#form’);
-// const email = document.querySelector(‘#mail’);
-// const errorHolder = document.querySelector(‘#error-message’);
-// function inputCheck() {
-//   // Get the values from the inputs
-//   const userEmail = email.value.trim();
-//   const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str); if (!isLowerCase(userEmail)) {
-//     // show error
-//     // add error class
-//     errorHolder.innerText = ‘Invalid Email! Emails must be written with lowercase characters.’;
-//   }
-// }
-// form.addEventListener(‘submit’, (e) => {
-//   e.preventDefault();
-//   inputCheck();
-// });
-
-
-// // const newString = (str) => str == str.toLowerCase()
-// // document.querySelector('.submit').addEventListener('click', (event) => {
-// //   if (!newString(email)) {
-// //     event.preventDefault();
-// //     error.textMessage = 'Sorry! Please use all lowercase.';
-// //     error.style.color = 'red';
-// //   };
-// // });
